f74c4ae4825a06d272a574c2e6d962ff385006fc
ceumartins <ceres.martins@simplesdecredito.com.br>
2022-03-31 17:03:46 -0300

    para aprovacao

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
 /**
  * @param {*} probes 
  */
- var arrayProbes = [
-    {macs: "C4:29:E5:0A:09:88", radio: "Rd 1"},
-    {macs: "28:A2:04:82:70:9A", radio: "Rd 1"},
-    {macs: "F8:AE:80:0A:BB:17", radio: "Rd 1"},
-    {macs: "C4:29:E5:0A:09:88", radio: "Rd 2"},
-    {macs: "28:A2:04:82:70:9A", radio: "Rd 2"},
-    {macs: "F8:AE:80:0A:BB:17", radio: "Rd 3"},
-    {macs: "F0:81:2D:E9:C7:64", radio: "Rd 3"},
-    {macs: "28:A2:04:82:70:9A", radio: "Rd 4"}]
-  
-  var radioMacs = arrayProbes.map(macsByRadio);
-  console.log(radioMacs);
-  
-  function macsByRadio(probes){
-    let item = { };
-    item.radio = probes.radio;
-    item.macs = probes.macs;
-    return item;
-    
-  }
 
+const probes = require("./probes.json")
+  
+const macsByRadio = (probes) => {
+    if (!probes) return []
+
+const radioValue = new Set()
+for (const item of probes) {
+  radioValue.add(item.radio)
+}
+const arrayRadio = [...radioValue.values()]
+const result = []
+
+for (const item1 of arrayRadio) {
+  const macs = []
+  for (const itemBase of probes) {
+    if (itemBase.radio === item1) {
+      macs.push(itemBase.mac)
+    }
+  }
+  result.push({
+    radio: item1,
+    macs
+  })
+}
+return result
+}
   module.exports = { macsByRadio };
